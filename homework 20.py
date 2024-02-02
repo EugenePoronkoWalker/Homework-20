bbf57435b50d2c9eb6af3373fbbd2cbbb92b0351
Eugene Poronko <eugenewalkerporonko@gmail.com>
2024-02-02 13:22:27 +0200

    Added ActionChains

--- homework 20.py
+++ homework 20.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 from helpers.xpath import mensa as x
 
@@ -18,109 +19,109 @@ start_id_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located
 start_id_button.click()
 
 ex1_a_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex1_a_xpath)))
-ex1_a_button.click()
+ActionChains(driver).move_to_element(ex1_a_button).click(ex1_a_button).perform()
 
 ex2_b_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex2_b_xpath)))
-ex2_b_button.click()
+ActionChains(driver).move_to_element(ex2_b_button).click(ex2_b_button).perform()
 
 ex3_c_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex3_c_xpath)))
-ex3_c_button.click()
+ActionChains(driver).move_to_element(ex3_c_button).click(ex3_c_button).perform()
 
 ex4_d_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex4_d_xpath)))
-ex4_d_button.click()
+ActionChains(driver).move_to_element(ex4_d_button).click(ex4_d_button).perform()
 
 ex5_e_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex5_e_xpath)))
-ex5_e_button.click()
+ActionChains(driver).move_to_element(ex5_e_button).click(ex5_e_button).perform()
 
 ex6_f_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex6_f_xpath)))
-ex6_f_button.click()
+ActionChains(driver).move_to_element(ex6_f_button).click(ex6_f_button).perform()
 
 ex7_a_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex7_a_xpath)))
-ex7_a_button.click()
+ActionChains(driver).move_to_element(ex7_a_button).click(ex7_a_button).perform()
 
 ex8_b_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex8_b_xpath)))
-ex8_b_button.click()
+ActionChains(driver).move_to_element(ex8_b_button).click(ex8_b_button).perform()
 
 ex9_c_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex9_c_xpath)))
-ex9_c_button.click()
+ActionChains(driver).move_to_element(ex9_c_button).click(ex9_c_button).perform()
 
 ex10_d_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex10_d_xpath)))
-ex10_d_button.click()
+ActionChains(driver).move_to_element(ex10_d_button).click(ex10_d_button).perform()
 
 ex11_e_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex11_e_xpath)))
-ex11_e_button.click()
+ActionChains(driver).move_to_element(ex11_e_button).click(ex11_e_button).perform()
 
 ex12_f_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex12_f_xpath)))
-ex12_f_button.click()
+ActionChains(driver).move_to_element(ex12_f_button).click(ex12_f_button).perform()
 
 ex13_a_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex13_a_xpath)))
-ex13_a_button.click()
+ActionChains(driver).move_to_element(ex13_a_button).click(ex13_a_button).perform()
 
 ex14_b_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex14_b_xpath)))
-ex14_b_button.click()
+ActionChains(driver).move_to_element(ex14_b_button).click(ex14_b_button).perform()
 
 ex15_c_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex15_c_xpath)))
-ex15_c_button.click()
+ActionChains(driver).move_to_element(ex15_c_button).click(ex15_c_button).perform()
 
 ex16_d_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex16_d_xpath)))
-ex16_d_button.click()
+ActionChains(driver).move_to_element(ex16_d_button).click(ex16_d_button).perform()
 
 ex17_e_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex17_e_xpath)))
-ex17_e_button.click()
+ActionChains(driver).move_to_element(ex17_e_button).click(ex17_e_button).perform()
 
 ex18_f_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex18_f_xpath)))
-ex18_f_button.click()
+ActionChains(driver).move_to_element(ex18_f_button).click(ex18_f_button).perform()
 
 ex19_a_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex19_a_xpath)))
-ex19_a_button.click()
+ActionChains(driver).move_to_element(ex19_a_button).click(ex19_a_button).perform()
 
 ex20_b_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex20_b_xpath)))
-ex20_b_button.click()
+ActionChains(driver).move_to_element(ex20_b_button).click(ex20_b_button).perform()
 
 ex21_c_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex21_c_xpath)))
-ex21_c_button.click()
+ActionChains(driver).move_to_element(ex21_c_button).click(ex21_c_button).perform()
 
 ex22_d_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex22_d_xpath)))
-ex22_d_button.click()
+ActionChains(driver).move_to_element(ex22_d_button).click(ex22_d_button).perform()
 
 ex23_e_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex23_e_xpath)))
-ex23_e_button.click()
+ActionChains(driver).move_to_element(ex23_e_button).click(ex23_e_button).perform()
 
 ex24_f_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex24_f_xpath)))
-ex24_f_button.click()
+ActionChains(driver).move_to_element(ex24_f_button).click(ex24_f_button).perform()
 
 ex25_a_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex25_a_xpath)))
-ex25_a_button.click()
+ActionChains(driver).move_to_element(ex25_a_button).click(ex25_a_button).perform()
 
 ex26_b_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex26_b_xpath)))
-ex26_b_button.click()
+ActionChains(driver).move_to_element(ex26_b_button).click(ex26_b_button).perform()
 
 ex27_c_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex27_c_xpath)))
-ex27_c_button.click()
+ActionChains(driver).move_to_element(ex27_c_button).click(ex27_c_button).perform()
 
 ex28_d_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex28_d_xpath)))
-ex28_d_button.click()
+ActionChains(driver).move_to_element(ex28_d_button).click(ex28_d_button).perform()
 
 ex29_e_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex29_e_xpath)))
-ex29_e_button.click()
+ActionChains(driver).move_to_element(ex29_e_button).click(ex29_e_button).perform()
 
 ex30_f_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex30_f_xpath)))
-ex30_f_button.click()
+ActionChains(driver).move_to_element(ex30_f_button).click(ex30_f_button).perform()
 
 ex31_a_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex31_a_xpath)))
-ex31_a_button.click()
+ActionChains(driver).move_to_element(ex31_a_button).click(ex31_a_button).perform()
 
 ex32_b_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex32_b_xpath)))
-ex32_b_button.click()
+ActionChains(driver).move_to_element(ex32_b_button).click(ex32_b_button).perform()
 
 ex33_c_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex33_c_xpath)))
-ex33_c_button.click()
+ActionChains(driver).move_to_element(ex33_c_button).click(ex33_c_button).perform()
 
 ex34_d_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex34_d_xpath)))
-ex34_d_button.click()
+ActionChains(driver).move_to_element(ex34_d_button).click(ex34_d_button).perform()
 
 ex35_e_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.ex35_e_xpath)))
-ex35_e_button.click()
+ActionChains(driver).move_to_element(ex35_e_button).click(ex35_e_button).perform()
 
 finish_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.finish_xpath)))
 finish_button.click()
@@ -128,4 +129,4 @@ finish_button.click()
 done_already_button = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, x.done_already_xpath)))
 done_already_button.click()
 
-time.sleep(3000)
+time.sleep(300)
